c20370c51580b2a4b4092a16b6febb6e4001e23c
Raja O <orajacse@gmail.com>
2019-09-19 20:06:05 +0530

    Content & Footer Component

--- new-app/src/app/app.component.ts
+++ new-app/src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { ContentAreaComponent } from './content-area/content-area.component'
+
 
 @Component({
   selector: 'app-root',
--- new-app/src/app/app.module.ts
+++ new-app/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavComponent } from './nav/nav.component';
+import { ContentAreaComponent } from './content-area/content-area.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavComponent
+    NavComponent,
+    ContentAreaComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
